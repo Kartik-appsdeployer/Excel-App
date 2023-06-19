defa4c5ac8777f5687be1618801a93e962816c43
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2023-06-19 14:43:03 +0530

    added

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+lse:
+            #     VL = sum(tot_d[i - 30:i])+1
+            #     tot_d.append(VL)
+            #     pr_d.append(sum(pr_d[i - 31:i]))
+            #     PH.append(sum(PH[i - 31:i]))
+            #     WO.append(sum(WO[i - 31:i]))
+            #     leave.append(sum(leave[i - 31:i]))
+            #     abs.append(sum(abs[i - 31:i]))
+            #     esic.append(sum(esic[i - 31:i]))
+            #     Late.appen
